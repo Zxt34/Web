0d2326381df0e1d7afa6ce29bf77e5a9b8c4be57
Zxt34 <3265923652@qq.com>
2021-07-28 22:12:39 +0800

    练习

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    static int a;
+    public static void main(String[] args) {
+//         int a;
+        int b = 0x10;
+        System.out.printf("%x\n",b << 1);
+        if(a == 0){
+            System.out.println("hello");
+        }
+    }
+}
