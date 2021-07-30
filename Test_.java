9c393a14e6cbf62b38797468f274b60f7dabb83b
Zxt34 <3265923652@qq.com>
2021-07-30 19:11:45 +0800

    练习

---
+++ Test_.java
@@ -0,0 +1,35 @@
+public class Test_ {
+//    static int a;
+
+//    static String str = new String();
+    static String str;
+    public static void main(String[] args) {
+//         int a;
+//        int b = 0x10;
+//        System.out.printf("%x\n",b << 1);
+//        if(a == 0){
+//            System.out.println("hello");
+//        }
+
+//        System.out.println("刘".compareTo("杨"));
+
+//        String str = "   hello  wor ld   ";
+//        System.out.println(str.replaceAll("l","_"));
+//        System.out.println(str.replaceFirst("l","_"));
+
+//        String[] result = str.split(" ",2);
+//        for (String s:result) {
+//            System.out.println(s);
+//        }
+
+//        System.out.println("[" + str.trim() + "]");
+//        System.out.println("[" + str + "]");
+
+//        System.out.println(str.length());
+
+//        System.out.println("".isEmpty());
+//        System.out.println(new String().isEmpty());
+
+        System.out.println(str);
+    }
+}
